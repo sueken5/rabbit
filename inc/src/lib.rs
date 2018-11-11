f9040fb011dc7b339048e09d69c04d8c67fa6620
sueken5 <sueken0117@gmail.com>
2018-11-11 18:42:05 +0900

    add print

--- inc/src/lib.rs
+++ inc/src/lib.rs
@@ -1,1 +1,5 @@
+#![no_std]
+#![feature(asm)]
+
+pub mod x86;
 pub mod elf;
---
+++ inc/src/x86.rs
@@ -0,0 +1,20 @@
+#[inline(always)]
+pub unsafe fn inb(port: u16) -> u8 {
+    let data: u8;
+    asm!("inb %dx, %al" : "={ax}" (data) : "{dx}"(port) :: "volatile");
+    return data;
+}
+
+#[inline(always)]
+pub unsafe fn insl(port: u16, addr: u32, cnt: u32) {
+    asm!("cld; rep insl %dx, (%edi)"
+         :
+         : "{ecx}"(cnt), "{dx}"(port), "{edi}"(addr)
+         : "ecx", "edi", "memory", "cc"
+         : "volatile");
+}
+
+#[inline(always)]
+pub unsafe fn outb(port: u16, data: u8) {
+    asm!("outb %al, %dx" :: "{dx}"(port), "{al}"(data) :: "volatile");
+}
