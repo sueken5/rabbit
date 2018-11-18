6d6a59adc5035ab360761f15a4e78c6821c53d29
sueken5 <sueken0117@gmail.com>
2018-11-18 11:14:39 +0900

    rename

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -1,7 +1,7 @@
 #![no_std]
 
 #[no_mangle]
-pub extern "C" fn main() {
+pub extern "C" fn kmain() {
     let vga_buffer = 0xb8000 as *mut u8;
 
     for (i, &byte) in b"Entry Kernel!".iter().enumerate() {
@@ -11,4 +11,10 @@ pub extern "C" fn main() {
         }
     }
     loop{}
+}
+
+#[panic_handler]
+#[no_mangle]
+pub extern "C" fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
